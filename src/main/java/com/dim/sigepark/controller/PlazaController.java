3ab223beb3e96511c7d6357c621fe0d3129a14ec
Itejsas <Ibatejsas@hotmail.es>
2020-06-02 21:06:36 +0200

    actualizando

--- src/main/java/com/dim/sigepark/controller/PlazaController.java
+++ src/main/java/com/dim/sigepark/controller/PlazaController.java
@@ -7,6 +7,8 @@ import org.springframework.data.rest.webmvc.RepositoryRestController;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.client.HttpClientErrorException;
+
 import com.dim.sigepark.entity.Plaza;
 import com.dim.sigepark.repository.PlazaDAO;
 
@@ -23,6 +25,8 @@ public class PlazaController {
 		if (plazas.size() > 0) {
 			Plaza plazaABorrar = plazas.get(plazas.size() - 1);
 			plazaDAO.delete(plazaABorrar);
+		}else {
+			throw new HttpClientErrorException(HttpStatus.PRECONDITION_FAILED, "No se pueden borrar mas plazas");
 		}
 	}
 
