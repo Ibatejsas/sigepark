0da91397c568077406d80cf7ef120263decf73c3
Itejsas <Ibatejsas@hotmail.es>
2020-06-29 18:41:03 +0200

    producto Minimo Viable

--- src/main/java/com/dim/sigepark/repository/TicketDAOCustom.java
+++ src/main/java/com/dim/sigepark/repository/TicketDAOCustom.java
@@ -4,6 +4,7 @@ import com.dim.sigepark.entity.Ticket;
 
 // utilizamos los custom para extender el JPA repository
 // definimos metodos propios a traves de un interface normal
+// sin uso
 
 public interface TicketDAOCustom<T extends Ticket> {
 
